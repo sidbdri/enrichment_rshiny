8d6d1bdc14f41c96431a2d9bcea92c1631479d43
katieemelianova <katie_emelianova@hotmail.com>
2020-03-10 12:48:34 +0000

    move scripts and test data to another file

--- app.R
+++ app.R
@@ -1,46 +1,9 @@
 library(shiny)
+source("functions.R")
 
-example_query<-c("1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at", "1786_at", "1112_g_at", "1331_s_at", "1355_g_at", "1372_at")
-example_reference<-c("1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at", "1786_at", "1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at")
-example_background<-c("1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at", "1786_at", "1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at", "1786_at", "1112_g_at", "1331_s_at", "1355_g_at", "1372_at", "1391_s_at", "1403_s_at", "1419_g_at", "1575_at", "1645_at", "1786_at")
-
-example_query<-paste(example_query, collapse="\n")
-example_reference<-paste(example_reference, collapse="\n")
-example_background<-paste(example_background, collapse="\n")
-
-
-calculate_intersect<-function(x, y){
-    i<-intersect(x, y)
-    return(i)
-}
-
-fisher_test<-function(contingency_tbl){
-    test_result<-fisher.test(contingency_tbl)
-    return(test_result)
-}
-
-contingency_table<-function(query, reference, background){
-    #lengths of each list
-    qlength<-length(query)
-    rlength<-length(reference)
-    blength<-length(background)
-    
-    # how many values are shared between query and reference
-    intersect_query_ref<-length(intersect(query, reference))
-    
-    # how many values are shared between query and background (not counting values intersecting with query)
-    intersect_query_back<-length(intersect(query, background)) - intersect_query_ref
-    
-    # how many values are shared between reference and background (not counting values intersecting with query)
-    intersect_ref_back<- length(intersect(reference, background)) - intersect_query_ref
-    
-    # how many values are in background and not in the query or reference
-    background_only<-blength - ((qlength + rlength) - intersect_query_ref)
-    
-    # make a contingency table of calculated values
-    contingency_table<-matrix(c(intersect_query_ref, intersect_query_back, intersect_ref_back, background_only), nrow=2)
-    return(contingency_table)
-}
+example_query<-read.delim("example_query")
+example_reference<-read.delim("example_reference")
+example_background<-read.delim("example_background")
 
 u <- shinyUI(
     fluidPage(
---
+++ functions.R
@@ -0,0 +1,32 @@
+calculate_intersect<-function(x, y){
+  i<-intersect(x, y)
+  return(i)
+}
+
+fisher_test<-function(contingency_tbl){
+  test_result<-fisher.test(contingency_tbl)
+  return(test_result)
+}
+
+contingency_table<-function(query, reference, background){
+  #lengths of each list
+  qlength<-length(query)
+  rlength<-length(reference)
+  blength<-length(background)
+  
+  # how many values are shared between query and reference
+  intersect_query_ref<-length(intersect(query, reference))
+  
+  # how many values are shared between query and background (not counting values intersecting with query)
+  intersect_query_back<-length(intersect(query, background)) - intersect_query_ref
+  
+  # how many values are shared between reference and background (not counting values intersecting with query)
+  intersect_ref_back<- length(intersect(reference, background)) - intersect_query_ref
+  
+  # how many values are in background and not in the query or reference
+  background_only<-blength - ((qlength + rlength) - intersect_query_ref)
+  
+  # make a contingency table of calculated values
+  contingency_table<-matrix(c(intersect_query_ref, intersect_query_back, intersect_ref_back, background_only), nrow=2)
+  return(contingency_table)
+}
